460bc195d0243cff1e3acf92d51155bed255cea3
Linus Lagerhjelm <linus.lagerhjelm@omegapoint.se>
2021-09-06 11:12:07 +0200

    feat: Make http call from GW to backend

--- api-gw/src/main/java/se/omegapoint/kompetensdag/apigw/GwController.java
+++ api-gw/src/main/java/se/omegapoint/kompetensdag/apigw/GwController.java
@@ -1,15 +1,28 @@
 package se.omegapoint.kompetensdag.apigw;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
-public class Controller {
+public class GwController {
+
+    private final GwService service;
+
+    @Autowired
+    public GwController(GwService service) {
+        this.service = service;
+    }
 
     @GetMapping("hello-client")
     public String helloFromClient() {
         return "Hello from client";
+    }
+
+    @GetMapping("hello-proxy")
+    public String proxyRequestToBackend() {
+        return service.callBackend();
     }
 }
