db924e65b76afcf106a59d17f4a7f48734947b7c
Linus Lagerhjelm <linus.lagerhjelm@omegapoint.se>
2021-09-07 15:25:40 +0200

    feat: Add implementation to UnauthorizedUserException

--- business-application/src/main/java/se/omegapoint/kompetensdag/businessapplication/UnauthorizedException.java
+++ business-application/src/main/java/se/omegapoint/kompetensdag/businessapplication/UnauthorizedException.java
@@ -1,2 +1,13 @@
-package se.omegapoint.kompetensdag.businessapplication;public class UnauthorizedException {
+package se.omegapoint.kompetensdag.businessapplication;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class UnauthorizedException extends AuthenticationException {
+    public UnauthorizedException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    public UnauthorizedException(String msg) {
+        super(msg);
+    }
 }
