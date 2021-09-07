b193eca66eb3a7d47270fc9470307670d301e6c9
Linus Lagerhjelm <linus.lagerhjelm@omegapoint.se>
2021-09-07 15:47:20 +0200

    fix: Call to backend service should not be in the base package

--- api-gw/src/main/java/se/omegapoint/kompetensdag/apigw/GwController.java
+++ api-gw/src/main/java/se/omegapoint/kompetensdag/apigw/GwController.java
@@ -1,6 +1,5 @@
 package se.omegapoint.kompetensdag.apigw;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,20 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api")
 public class GwController {
 
-    private final GwService service;
-
-    @Autowired
-    public GwController(GwService service) {
-        this.service = service;
-    }
-
     @GetMapping("hello-client")
     public String helloFromClient() {
         return "Hello from client";
     }
 
-    @GetMapping("hello-proxy")
-    public String proxyRequestToBackend() {
-        return service.callBackend();
-    }
 }
